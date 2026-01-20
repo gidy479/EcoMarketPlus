aa81552c61c94a9443805c61a4f187a075451ad2
Gideon <gideontetteh792@gmail.com>
2026-01-20 00:02:42 +0000

    Config updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://gideontetteh792:hVga035qnLY43vsn@cluster0.tzlhh.mongodb.net/ecomarketplus?appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://gideontetteh792:iQ41ucs9FMxGUCeP@cluster0.tzlhh.mongodb.net/?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
